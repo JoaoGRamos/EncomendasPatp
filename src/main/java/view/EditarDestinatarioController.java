520870b111cdb6aa6138c8e0ddcd950fd4749ed8
EdersonPB <edersonbassoli@hotmail.com>
2022-06-07 13:44:55 -0300

    telas beta(1.1.1)

--- src/main/java/view/EditarDestinatarioController.java
+++ src/main/java/view/EditarDestinatarioController.java
@@ -1,4 +1,42 @@
 package view;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
+
 public class EditarDestinatarioController {
+
+    @FXML
+    private Button btCancelar;
+
+    @FXML
+    private Button btSalvar;
+
+    @FXML
+    private ComboBox<?> cbID;
+
+    @FXML
+    private TextField tfCpf;
+
+    @FXML
+    private TextField tfEndereco;
+
+    @FXML
+    private TextField tfNome;
+
+    @FXML
+    private TextField tfTelefone;
+
+    @FXML
+    void acaoCancelar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void acaoSalvar(ActionEvent event) {
+
+    }
+
 }
--- src/main/java/view/EditarRemetenteController.java
+++ src/main/java/view/EditarRemetenteController.java
@@ -1,4 +1,39 @@
 package view;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
 public class EditarRemetenteController {
+
+    @FXML
+    private Button btCancelar;
+
+    @FXML
+    private Button btSalvar;
+
+    @FXML
+    private AnchorPane cbID;
+
+    @FXML
+    private TextField tfCpf;
+
+    @FXML
+    private TextField tfNome;
+
+    @FXML
+    private TextField tfTelefone;
+
+    @FXML
+    void acaoCancelar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void acaoSalvar(ActionEvent event) {
+
+    }
+
 }
