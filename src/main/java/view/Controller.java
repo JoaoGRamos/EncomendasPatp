29a71effd6f23919952a5d61da440dfda9d70e01
Joao Ramos <joaoramos552@gmail.com>
2022-06-07 09:41:11 -0300

    2 tela teste

--- src/main/java/view/Controller.java
+++ src/main/java/view/Controller.java
@@ -3,11 +3,18 @@ package view;
 import dao.UnidadesDAO;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.*;
+import javafx.stage.Stage;
 import model.Unidades;
 import dao.UnidadesDAO;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -43,5 +50,17 @@ public class Controller {
 
     }
 
-    
+    @FXML
+    void acaoBotao(ActionEvent event) throws IOException {
+        Stage cadastro = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("/view/CentralController.fxml"));
+        cadastro = (Stage)((Node)event.getSource()).getScene().getWindow();
+        cadastro.setTitle("Rastreio de Encomendas");
+        cadastro.setScene(new Scene(root));
+        cadastro.show();
+
+    }
+
+
+
 }
