ae2fcebf6e3648d798a116b6d77d43d84fc25252
Joao Ramos <joaoramos552@gmail.com>
2022-06-09 14:06:01 -0300

    tela inicial nao fecha

--- src/main/java/view/CentralController.java
+++ src/main/java/view/CentralController.java
@@ -8,12 +8,9 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.control.MenuBar;
 import javafx.scene.control.MenuItem;
-import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 
-
-import java.io.File;
 import java.io.IOException;
 
 public class CentralController {
@@ -46,7 +43,11 @@ public class CentralController {
     private Label nomeUsuario;
 
     @FXML
+    private MenuItem miCadEncomendas;
+
+    @FXML
     private ImageView img;
+
     @FXML
     private MenuBar mbPrincipal;
 
@@ -55,9 +56,17 @@ public class CentralController {
     private Parent root;
 
     public void initialize() {
-        File file = new File("logotop.png");
-        Image image = new Image(file.toURI().toString());
-        img.setImage(image);
+
+    }
+
+    @FXML
+    void acaoCadEncomendas(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/view/despachefx.fxml"));
+        Stage st = new Stage();
+//        stage = (Stage) mbPrincipal.getScene().getWindow();
+        st.setTitle("Cadastrar Encomenda");
+        st.setScene(new Scene(root));
+        st.show();
     }
 
     @FXML
@@ -65,7 +74,7 @@ public class CentralController {
         Parent root = FXMLLoader.load(getClass().getResource("/view/destinatariofx.fxml"));
         Stage st = new Stage();
 //        stage = (Stage) mbPrincipal.getScene().getWindow();
-        st.setTitle("Cadastro de Destinatario!");
+        st.setTitle("Cadastro de Destinatario");
         st.setScene(new Scene(root));
         st.show();
 
@@ -74,64 +83,71 @@ public class CentralController {
     @FXML
     void addRemetente(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("/view/remetentefx.fxml"));
-        stage = (Stage) mbPrincipal.getScene().getWindow();
-        stage.setTitle("Cadastro de Remetente!");
-        stage.setScene(new Scene(root));
-        stage.show();
+        Stage st = new Stage();
+//        stage = (Stage) mbPrincipal.getScene().getWindow();
+        st.setTitle("Cadastro de Remetente");
+        st.setScene(new Scene(root));
+        st.show();
 
     }
 
     @FXML
     void edDestinatario(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("/view/editardestinatariofx.fxml"));
-        stage = (Stage) mbPrincipal.getScene().getWindow();
-        stage.setTitle("Editar Destinatario!");
-        stage.setScene(new Scene(root));
-        stage.show();
+        Stage st = new Stage();
+//        stage = (Stage) mbPrincipal.getScene().getWindow();
+        st.setTitle("Editar Destinatario");
+        st.setScene(new Scene(root));
+        st.show();
 
     }
 
     @FXML
     void edRemetente(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("/view/editarremetentefx.fxml"));
-        stage = (Stage) mbPrincipal.getScene().getWindow();
-        stage.setTitle("Editar Remetente!");
-        stage.setScene(new Scene(root));
-        stage.show();
+        Stage st = new Stage();
+//        stage = (Stage) mbPrincipal.getScene().getWindow();
+        st.setTitle("Editar Remetente");
+        st.setScene(new Scene(root));
+        st.show();
     }
     @FXML
     void acaoDespache(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("/view/despachefx.fxml"));
-        stage = (Stage) mbPrincipal.getScene().getWindow();
-        stage.setTitle("Editar Remetente!");
-        stage.setScene(new Scene(root));
-        stage.show();
+        Stage st = new Stage();
+//        stage = (Stage) mbPrincipal.getScene().getWindow();
+        st.setTitle("Cadastrar Encomenda");
+        st.setScene(new Scene(root));
+        st.show();
     }
 
     @FXML
     void acaoRastreio(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("/view/rastreiofx.fxml"));
-        stage = (Stage) mbPrincipal.getScene().getWindow();
-        stage.setTitle("Editar Remetente!");
-        stage.setScene(new Scene(root));
-        stage.show();
+        Stage st = new Stage();
+//        stage = (Stage) mbPrincipal.getScene().getWindow();
+        st.setTitle("Rastrear Encomendas");
+        st.setScene(new Scene(root));
+        st.show();
     }
 
     @FXML
     void acaoRotas(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("/view/rotasfx.fxml"));
-        stage = (Stage) mbPrincipal.getScene().getWindow();
-        stage.setTitle("Editar Remetente!");
-        stage.setScene(new Scene(root));
-        stage.show();
+        Stage st = new Stage();
+//        stage = (Stage) mbPrincipal.getScene().getWindow();
+        st.setTitle("Rotas");
+        st.setScene(new Scene(root));
+        st.show();
     }
 
     @FXML
     void acaoSobre(ActionEvent event) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("/view/sobrefx.fxml"));
-        stage = (Stage) mbPrincipal.getScene().getWindow();
-        stage.setTitle("Editar Remetente!");
-        stage.setScene(new Scene(root));
-        stage.show();
+        Stage st = new Stage();
+//        stage = (Stage) mbPrincipal.getScene().getWindow();
+        st.setTitle("Sobre Rastreio de Encomendas");
+        st.setScene(new Scene(root));
+        st.show();
     }
 }
