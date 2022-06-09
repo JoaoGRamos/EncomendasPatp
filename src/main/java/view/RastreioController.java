4a0d6dc0eeaeb6832b8a2f29ad521d32ef3c0211
EdersonPB <edersonbassoli@hotmail.com>
2022-06-08 21:25:33 -0300

    tela rastreio(1.0)

--- src/main/java/view/RastreioController.java
+++ src/main/java/view/RastreioController.java
@@ -1,4 +1,48 @@
 package view;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TextField;
+
 public class RastreioController {
+
+    @FXML
+    private Button btPesquisar;
+
+    @FXML
+    private Button btVoltar;
+
+    @FXML
+    private TableColumn<?, ?> tcDataEntrada;
+
+    @FXML
+    private TableColumn<?, ?> tcDataSaida;
+
+    @FXML
+    private TableColumn<?, ?> tcDestino;
+
+    @FXML
+    private TableColumn<?, ?> tcLocal;
+
+    @FXML
+    private TableColumn<?, ?> tcOrigem;
+
+    @FXML
+    private TableColumn<?, ?> tcStatus;
+
+    @FXML
+    private TextField tfPesquisa;
+
+    @FXML
+    void acaoPesquisar(ActionEvent event) {
+
+    }
+
+    @FXML
+    void acaoVoltar(ActionEvent event) {
+
+    }
+
 }
