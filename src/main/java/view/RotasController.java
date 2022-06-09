1e51f54688fc2ff727ee0b458bb1c4fda01effb8
EdersonPB <edersonbassoli@hotmail.com>
2022-06-09 17:21:39 -0300

    rotas rotas controller (1.0)

--- src/main/java/view/RotasController.java
+++ src/main/java/view/RotasController.java
@@ -1,4 +1,29 @@
 package view;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.stage.Stage;
+
 public class RotasController {
+
+    @FXML
+    private Button btVoltar;
+
+    @FXML
+    private TableColumn<?, ?> tcDestino;
+
+    @FXML
+    private TableColumn<?, ?> tcId;
+
+    @FXML
+    private TableColumn<?, ?> tcOrigem;
+
+    @FXML
+    void acaoVoltar(ActionEvent event) {
+        Stage stage = (Stage) btVoltar.getScene().getWindow(); //Obtendo a janela atual
+        stage.close(); //Fechando o Stage
+    }
+
 }
